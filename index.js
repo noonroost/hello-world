a4bc45e655944662ebb20fff16a4a171188e4e04
noonroost <noon.roost@gmail.com>
2024-04-29 09:15:54 +0200

    world

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const isRGBColor = (rgb) => {
   const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
   return regex.test(rgb);
 }
+if (localStorage.getItem('username')) {
+    console.log('Item exists!');
+}
