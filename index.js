3991ad584bf0f840f8d7d9ccead75a9c6a3f8505
noonroost <noon.roost@gmail.com>
2024-04-29 09:14:03 +0200

    world

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const string = 'Hello world'
 console.log(string)
 
 alert(string)
-
+const isRGBColor = (rgb) => {
+  const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
+  return regex.test(rgb);
+}
