7c9c016cd5df549f463c2e8821a4ed3ccb07797d
noonroost <noon.roost@gmail.com>
2024-05-14 08:51:37 +0200

    Tue May 14 08:51:37 CEST 2024

--- index.js
+++ index.js
@@ -64,3 +64,7 @@ const isEthereumAddress = (address) => {
   const regex = /0x[a-fA-F0-9]{40}/;
   return regex.test(address);
 }
+const isEthereumAddress = (address) => {
+  const regex = /0x[a-fA-F0-9]{40}/;
+  return regex.test(address);
+}
