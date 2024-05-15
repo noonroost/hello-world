c63db4acf36e80dadc2e268e5d0b52407fbf38e0
noonroost <noon.roost@gmail.com>
2024-05-15 07:05:06 +0200

    Wed May 15 07:05:06 CEST 2024

--- index.js
+++ index.js
@@ -68,3 +68,15 @@ const isEthereumAddress = (address) => {
   const regex = /0x[a-fA-F0-9]{40}/;
   return regex.test(address);
 }
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
