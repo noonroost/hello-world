086bbe7a2c4ddbb97d99418b02045bcc06a81270
noonroost <noon.roost@gmail.com>
2024-05-26 09:19:02 +0200

    Sun May 26 09:19:02 CEST 2024

--- index.js
+++ index.js
@@ -132,3 +132,6 @@ const isRippleAddress = (address) => {
 }
 console.log(localStorage.getItem('username'));
 const array = [...Array(10).keys()].map(i => i + 1);
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
