e49a6a37b7e6d2f88b76ff7e02748805a91e5299
noonroost <noon.roost@gmail.com>
2024-06-05 08:10:53 +0200

    Wed Jun  5 08:10:53 CEST 2024

--- index.js
+++ index.js
@@ -162,3 +162,6 @@ const isRGBColor = (rgb) => {
   const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
   return regex.test(rgb);
 }
+function countOccurences(str, char) {
+  return str.split(char).length - 1;
+}
