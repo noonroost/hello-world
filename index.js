eee73119eeb859afa1a85b80d8ae1c1becc99bf7
noonroost <noon.roost@gmail.com>
2024-06-20 07:32:31 +0200

    Thu Jun 20 07:32:31 CEST 2024

--- index.js
+++ index.js
@@ -232,3 +232,8 @@ el.addEventListener('keyup', e => {
     ? 'display: block'
     : 'display: none';
 });
+if (/\S+@\S+\.\S+/.test('david@codeshack.io')) {
+    console.log('Email is valid!');
+} else {
+    console.log('Email is invalid!');
+}
