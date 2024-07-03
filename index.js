d34da80f47d8c61655994d88ff98db4d5ee679cd
noonroost <noon.roost@gmail.com>
2024-07-03 07:30:05 +0200

    Wed Jul  3 07:30:05 CEST 2024

--- index.js
+++ index.js
@@ -289,3 +289,15 @@ function countOccurences(str, char) {
   return str.split(char).length - 1;
 }
 let reversedStr = str.split("").reverse().join("");
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
