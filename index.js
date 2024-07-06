c7d16e27ee1b0440fe0e1c904a6d6a90cfd0c994
noonroost <noon.roost@gmail.com>
2024-07-06 22:15:33 +0200

    Sat Jul  6 22:15:33 CEST 2024

--- index.js
+++ index.js
@@ -315,3 +315,15 @@ function countdownTimer(minutes) {
     }
   }, 1000);
 }
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
