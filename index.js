4b82bd17e701e43ad9cd25dace1677af20d1ef21
noonroost <noon.roost@gmail.com>
2024-07-09 07:15:46 +0200

    Tue Jul  9 07:15:46 CEST 2024

--- index.js
+++ index.js
@@ -331,3 +331,8 @@ const currentUrl = window.location.href;
 function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+function toTitleCase(str) {
+  return str.replace(/\w/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
