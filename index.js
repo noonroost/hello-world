92df41af83525984f9e609c5d9aa9ad0121d9c6e
noonroost <noon.roost@gmail.com>
2024-07-16 08:51:36 +0200

    Tue Jul 16 08:51:36 CEST 2024

--- index.js
+++ index.js
@@ -383,3 +383,11 @@ const httpsRedirect = () => {
   if (location.protocol !== 'https:')
     location.replace('https://' + location.href.split('//')[1]);
 };
+const btn = document.querySelector('a[data-btn]');
+btn.onclick = event => {
+    event.preventDefault();
+    // Output data attributes
+    console.log(btn.dataset.name); // David
+    console.log(btn.dataset.surname); // Adams
+    console.log(btn.dataset.uniqueId); // 30
+};
