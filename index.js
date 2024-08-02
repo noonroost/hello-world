05906c998d4eb3f1a928d5785eea3ac05e1dab03
noonroost <noon.roost@gmail.com>
2024-08-02 07:50:56 +0200

    Fri Aug  2 07:50:55 CEST 2024

--- index.js
+++ index.js
@@ -531,3 +531,10 @@ class Fruit {
     }   
 
 }
+const debounce = (fn, time) => {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this, args), time);
+  };
+};
