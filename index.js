d6293cc4427f6585ab01be06d1d4bc7c627ac23d
noonroost <noon.roost@gmail.com>
2024-08-05 07:31:46 +0200

    Mon Aug  5 07:31:46 CEST 2024

--- index.js
+++ index.js
@@ -542,3 +542,14 @@ console.log(localStorage.getItem('username'));
 const openTab = (url) => {
   window.open(url, "_blank");
 };
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
