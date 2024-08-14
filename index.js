abdeaa81f571065ec487d96409bdc91f7566c032
noonroost <noon.roost@gmail.com>
2024-08-14 07:46:58 +0200

    Wed Aug 14 07:46:58 CEST 2024

--- index.js
+++ index.js
@@ -590,3 +590,14 @@ if (/\S+@\S+\.\S+/.test('david@codeshack.io')) {
 } else {
     console.log('Email is invalid!');
 }
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
