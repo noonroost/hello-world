6cea6f3f545b99ac018991b3530fc3c23eb5bbc4
noonroost <noon.roost@gmail.com>
2024-08-19 07:47:05 +0200

    Mon Aug 19 07:47:05 CEST 2024

--- index.js
+++ index.js
@@ -618,3 +618,7 @@ function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+const isEthereumAddress = (address) => {
+  const regex = /0x[a-fA-F0-9]{40}/;
+  return regex.test(address);
+}
