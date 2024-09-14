8655e9d3fd460544e164924a93af346a526d16f3
noonroost <noon.roost@gmail.com>
2024-09-14 14:10:27 +0200

    Sat Sep 14 14:10:26 CEST 2024

--- index.js
+++ index.js
@@ -739,3 +739,7 @@ const isRippleAddress = (address) => {
 let uniqueArr = [...new Set(arr)];
 let reversedStr = str.split("").reverse().join("");
 let reversedStr = str.split("").reverse().join("");
+const isCreditCard = (cc) => {
+  const regex = /(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/;
+  return regex.test(cc);
+}
