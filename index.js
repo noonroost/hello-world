24df26088ee6a53a2e55367a473ae98e08581790
noonroost <noon.roost@gmail.com>
2024-09-24 09:03:50 +0200

    Tue Sep 24 09:03:50 CEST 2024

--- index.js
+++ index.js
@@ -775,3 +775,7 @@ const debounce = (fn, time) => {
     timeout = setTimeout(() => fn.apply(this, args), time);
   };
 };
+const isRGBColor = (rgb) => {
+  const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
+  return regex.test(rgb);
+}
