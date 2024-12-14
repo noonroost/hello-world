b397c01b97f080bbfc908493b0128961ee16da25
noonroost <noon.roost@gmail.com>
2024-12-14 13:47:00 +0100

    Sat Dec 14 13:47:00 CET 2024

--- index.js
+++ index.js
@@ -847,3 +847,14 @@ const randomString = (length) => {
   }
   return result;
 };
+function countdownTimer(minutes) {
+  let seconds = minutes * 60;
+  const countdown = setInterval(function() {
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(countdown);
+    } else {
+      console.log(seconds + " seconds left");
+    }
+  }, 1000);
+}
