8b907c183adfb237a825eeca1ebab9e4c3f29cf8
noonroost <noon.roost@gmail.com>
2024-12-17 08:01:51 +0100

    Tue Dec 17 08:01:51 CET 2024

--- index.js
+++ index.js
@@ -862,3 +862,15 @@ const currentUrl = window.location.href;
 const openTab = (url) => {
   window.open(url, "_blank");
 };
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
